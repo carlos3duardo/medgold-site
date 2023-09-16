0f33b5db02ee91fedb0a47c7304aac66a5635ee0
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2023-09-16 19:31:27 -0300

    feat: Layout do menu dinâmico Ao rolar a página, o cabeçalho fica com o fundo branco.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,36 +1,33 @@
+'use client';
 import Image from 'next/image';
 import logo from '@/assets/images/header-logo-medgold.png';
-
-type NavItem = {
-  href: string;
-  label: string;
-};
+import { menu } from './menu';
+import { useEffect, useState } from 'react';
 
 export function Header() {
-  const menu = [
-    {
-      href: '#o-que-e',
-      label: 'O que é',
-    },
-    {
-      href: '#beneficios',
-      label: 'Benefícios',
-    },
-    {
-      href: '#como',
-      label: 'Como',
-    },
-    {
-      href: '#cartao-digital',
-      label: 'Cartão Digital',
-    },
-    {
-      href: '#contrate',
-      label: 'Contrate agora',
-    },
-  ] as NavItem[];
+  const [solidBg, setSolidBg] = useState(false);
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+
+    setSolidBg(position > 240);
+
+    console.info({ position });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed top-0 left-0 w-full z-10">
+    <header
+      data-solid-background={solidBg}
+      className="fixed top-0 left-0 w-full z-10 transition-all duration-200 data-[solid-background=true]:bg-white data-[solid-background=true]:shadow-md"
+    >
       <div className="container mx-auto">
         <div className="flex items-center justify-between py-10">
           <figure>
@@ -39,15 +36,17 @@ export function Header() {
               width={184}
               height={54}
               alt="Logotipo MedGold"
-              style={{ filter: 'brightness(0) invert(1)' }}
+              data-solid-background={solidBg}
+              className="brightness-0 invert data-[solid-background=true]:brightness-100 data-[solid-background=true]:invert-0"
             />
           </figure>
           <div role="navigation" className="flex gap-2 items-center">
             {menu.map((item) => (
               <a
                 key={item.href}
-                className="relative py-2 px-3 font-semibold text-sm text-white uppercase transition z-[1] hover:text-primary-600 after:content-[''] after:opacity-25 after:block after:h-[1px] after:absolute after:bottom-0 after:left-[1rem] after:right-[1rem] after:bg-white after:transition-all after:origin-bottom after:z-[-1] hover:after:right-0 hover:after:left-0 hover:after:h-full hover:after:rounded-sm hover:after:opacity-100"
                 href={item.href}
+                data-solid-background={solidBg}
+                className="relative py-2 px-3 font-semibold text-sm text-white uppercase transition z-[1] hover:text-primary-600 after:content-[''] after:opacity-25 after:block after:h-[1px] after:absolute after:bottom-0 after:left-[1rem] after:right-[1rem] after:bg-white after:transition-all after:origin-bottom after:z-[-1] hover:after:right-0 hover:after:left-0 hover:after:h-full hover:after:rounded-sm hover:after:opacity-100 data-[solid-background=true]:text-primary-600 data-[solid-background=true]:after:bg-primary-600 data-[solid-background=true]:hover:text-white"
               >
                 {item.label}
               </a>
---
+++ src/components/Header/menu.ts
@@ -0,0 +1,27 @@
+type NavItem = {
+  href: string;
+  label: string;
+};
+
+export const menu: NavItem[] = [
+  {
+    href: '#o-que-e',
+    label: 'O que é',
+  },
+  {
+    href: '#beneficios',
+    label: 'Benefícios',
+  },
+  {
+    href: '#como',
+    label: 'Como',
+  },
+  {
+    href: '#cartao-digital',
+    label: 'Cartão Digital',
+  },
+  {
+    href: '#contrate',
+    label: 'Contrate agora',
+  },
+];
